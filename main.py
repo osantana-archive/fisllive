eca1017093920fd13bec3bb26863a00f47b18c2f
Osvaldo Santana <osantana@triveos.com>
2010-07-19 01:24:19 -0300

    Showing a template file.

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+from os.path import *
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
-
+from google.appengine.ext.webapp import template
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        template_values = {}
 
+        path = join(dirname(__file__), 'index.html')
+        rendered = template.render(path, template_values)
+        self.response.out.write(rendered)
 
-def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
+if __name__ == "__main__":
+    application = webapp.WSGIApplication([
+        ('/', MainHandler)
+    ], debug=True)
     util.run_wsgi_app(application)
-
-
-if __name__ == '__main__':
-    main()
