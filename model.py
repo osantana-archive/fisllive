a200761498dc18f9ec10f545152894abe2ad9aa4
Osvaldo Santana <osantana@triveos.com>
2010-07-21 16:32:02 -0300

    presentation fixes

--- model.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import db
-
-class Message(db.Model):
-    timestamp = db.DateTimeProperty(auto_now=True,
-                                    auto_now_add=True)
-    message = db.StringProperty(multiline=True)
-
